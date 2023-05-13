329069d35dfd0ada22b8877df656f9cb9e94a005
P-Draper <draperpa@gmail.com>
2023-05-13 11:49:33 -0400

    completed lab

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function increment(n) {
+    let number = n+1
+    return number;
+}
+function decrement(n) {
+    let number = n-1
+    return number;
+}
+function makeInt(n) {
+    let number = parseInt(n, 10);
+    return number;
+}
+function preserveDecimal(n) {
+    let number = parseFloat(n);
+    return number;
+}
